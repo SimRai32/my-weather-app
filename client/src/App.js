255a8af346f8df447a6d817fbf7f3c20f330a1f8
Simran Kalirai <100382994+SimRai32@users.noreply.github.com>
2024-04-18 16:01:43 -0700

    fixed error where function was called before it was created

--- client/src/App.js
+++ client/src/App.js
@@ -27,6 +27,12 @@ function App() {
     setOpenModal(false);
   }, []);
 
+  const createAlert = useCallback((message, alertTyping = null) => {
+    setAlertMessage(message);
+    setAlertType(alertTyping);
+    setOpenSnackBar(true);
+  }, []);
+
   useEffect(() => {
     if (!navigator?.geolocation) {
       setAutoRefresh(true);
@@ -45,7 +51,7 @@ function App() {
         },
         (error) => {
           const message = ("Error getting user location:", error);
-          createAlert(message, "error");
+          console.error(message);
         }
       );
     };
@@ -80,12 +86,6 @@ function App() {
       setAlertMessage(null);
       setAlertType(null);
     }, 3000);
-  }, []);
-
-  const createAlert = useCallback((message, alertTyping = null) => {
-    setAlertMessage(message);
-    setAlertType(alertTyping);
-    setOpenSnackBar(true);
   }, []);
 
   return (
